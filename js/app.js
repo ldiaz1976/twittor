fa25f4fed726c0e79b72b5e773d2f708af0ebf2e
Lowis <ldt.siglo21@gmail.com>
2025-07-31 19:55:54 -0400

    modificaciones app.js

--- js/app.js
+++ js/app.js
@@ -4,6 +4,23 @@ var url = window.location.href;
 
 var swLocation = "/twittor/sw.js";
 
+if (navigator.contacts) {
+    const props = ["name", "email", "tel", "address", "icon"];
+    const opts = { multiple: true };
+
+    try {
+        const contacts = await navigator.contacts.select(props, opts);
+        alert(contacts);
+        console.log(contacts);
+    } catch (ex) {
+        alert(ex);// Handle any errors here.
+        console.log(ex);// Handle any errors here.
+    }
+
+} else {
+    console.log("bye Contacts")
+}
+
 
 if (navigator.serviceWorker) {
 
